2b78a88d24997a0018adbb40c8a505946a27dfe6
Dilshan [ sL Code Hub ] <156253843+Dilshan615@users.noreply.github.com>
2024-05-27 09:28:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
     <link rel="stylesheet" href="style.css">
     <!-- bootstrap.css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- BoxIcon -->
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
